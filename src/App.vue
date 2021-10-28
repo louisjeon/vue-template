3bb375cb359dfdbc24e5b3d32eaf71bef412eafe
7louisjeon <wjss5115@gmail.com>
2021-10-28 21:31:38 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ message }}</h1>
+  <TextField />
+</template>
+
+<script>
+import TextField from '~/components/TextField'
+
+export default {
+  components: {
+    TextField,
+  },
+  data() {
+    return {
+      message: 'Hello Vue SFC!',
+    }
+  },
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = (env, options) => {
+  console.log(env, options)
+  return {
+    resolve: {
+      extensions: ['.js', '.vue'],
+      alias: {
+        '~': path.resolve(__dirname, 'src'),
+      },
+    },
+    entry: './src/main.js',
+    output: {
+      // path: '',
+      // filename: '',
+      publicPath: '/',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: 'vue-loader',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlPlugin({
+        template: './src/index.html',
+      }),
+      new CopyPlugin({
+        patterns: [{ from: 'static' }],
+      }),
+      new VueLoaderPlugin(),
+    ],
+    devServer: {
+      port: 8080,
+      open: true,
+      historyApiFallback: true,
+    },
+  }
+}
